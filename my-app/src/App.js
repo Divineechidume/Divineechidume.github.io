dc4ee330485d60fc02f7a8cbb14baa522f5c5d34
Divineechidume <divineechidume@berkeley.edu>
2021-02-27 03:08:13 -0800

    Added mybackground image and profile picture. I can now view background image. need to finalize code to be able to see profile picture

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Background from './images/Background.png';
+import DivineeCover from './images/DivineeCover.jpg';
+import './App.css';
+
+const divStyle = {
+  height: '100vh',
+  backgroundImage: `url(${Background})`,
+  backgroundPosition: "center",
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundAttachment: "fixed"
+};
+
+const rightPic = {
+  height: '100%',
+  backgroundImage: `url(${DivineeCover})`,
+  backgroundPosition: "right"
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
+       <div style={divStyle}>
+       	<div>
+            <div style={rightPic}>
+        	</div>
+	    </div>
+       </div>
     );
   }
 }
