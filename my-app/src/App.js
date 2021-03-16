1c5a7ff9f6b3815654c4eeb6bf4ae2d21297eac9
Divineechidume <divineechidume@berkeley.edu>
2021-03-15 22:53:57 -0700

    Added Navagation bar and made it into a reusable component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from "react";
-import Background from "./images/Background.svg";
-import DivineeCover from "./images/DivineeCover.jpg";
 import "./App.css";
-
-const divStyle = {
-  height: "100vh",
-  backgroundImage: `url(${Background})`,
-  backgroundPosition: "center",
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  backgroundAttachment: "fixed",
-};
+import "./containers/NavagationBar";
+import NavagationBar from "./containers/NavagationBar";
 
 class App extends Component {
   render() {
     return (
-      <div style={divStyle}>
+      <div class="background">
         <div class="container-fluid">
           <div class="row">
             <div class="col">
-              <nav class="navbar navbar-expand-lg navbar-transparent">
-                <a class="navbar-brand" href="#">
-                  Bootstrap
-                </a>
-              </nav>
+              <NavagationBar />
             </div>
-            <div class="col-sm-4 img-6" style={{ paddingRight: 0 }}></div>
+            <div
+              class="col-sm-4 divineeCover"
+              style={{ paddingRight: 0 }}
+            ></div>
           </div>
         </div>
       </div>
