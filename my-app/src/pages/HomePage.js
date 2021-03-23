87fc2a2262a3190cdf7df979e36763c623cae241
Divineechidume <divineechidume@berkeley.edu>
2021-03-22 23:07:24 -0700

    home page columns are horizontally stacked on mobiile devices

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -5,22 +5,24 @@ import Navbar from "../components/Navbar/Navbar.js";
 class HomePage extends Component {
   render() {
     return (
-      <div class="background">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col">
-              <Navbar />
-              <div div class="container">
-                <div class="mainText">DIVINEE CHIDUME</div>
-                <div class="subHeading">
-                  SOFTWARE ENGINEER - ENTREPRENUER - LIFE LEARNER
+      <div class="homeWrapper">
+        <div class="background">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col">
+                <Navbar />
+                <div div class="container">
+                  <div class="mainText">DIVINEE CHIDUME</div>
+                  <div class="subHeading">
+                    SOFTWARE ENGINEER - ENTREPRENUER - LIFE LEARNER
+                  </div>
                 </div>
               </div>
+              <div
+                class="col-xl divineeCover"
+                style={{ paddingRight: 0 }}
+              ></div>
             </div>
-            <div
-              class="col-sm-4 divineeCover"
-              style={{ paddingRight: 0 }}
-            ></div>
           </div>
         </div>
       </div>
