2e259f2ee2ac0e5d6ecfacc89376ce285357577c
Divineechidume <divineechidume@berkeley.edu>
2021-03-22 21:41:06 -0700

    added react routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage.js";
 class App extends Component {
   render() {
     return (
-      <div>
-        <HomePage />
-      </div>
+      <Router>
+        <div>
+          <switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+          </switch>
+        </div>
+      </Router>
     );
   }
 }
